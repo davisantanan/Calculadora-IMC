feba08309f9e1ecc1cbed223189e114eb2cd5af4
Davi Santana <santananogueira13@hotmail.com>
2023-03-02 10:57:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('button');
+const peso = document.getElementById('peso');
+const altura = document.getElementById('altura');
+const resultadoIMC = document.getElementById('resultado-imc');
+const resultadoText = document.getElementById('resultado-text');
+
+const getText = (imc) => {
+    if(imc > 40) return 'Obesidade grau III';
+    if(imc > 35) return 'Obesidade grau II';
+    if(imc > 30) return 'Obesidade grau I';
+    if(imc > 25) return 'Levemente acima do peso';
+    if(imc > 18.5) return 'Peso ideal';
+    return 'Abaixo do peso';
+}
+
+
+const imcCalc = () => {
+    if(!peso.value  || !altura.value) return resultadoIMC.textContent = 'Preencha todos os campos'
+    const valorImc = (+peso.value / (+altura.value * +altura.value)).toFixed(1);
+    resultadoIMC.textContent = `IMC de ${valorImc}`;
+    resultadoText.textContent = getText(valorImc);
+}
+
+button.addEventListener("click", imcCalc);
